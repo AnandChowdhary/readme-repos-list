a4e2c0e88d787f79ec342cec01b417d8c612a213
Anand Chowdhary <mail@anandchowdhary.com>
2020-11-05 17:14:43 +0530

    :goal_net: Log instead of throwing error

--- src/replace.ts
+++ src/replace.ts
@@ -1,6 +1,6 @@
 export const replaceContents = (start: string, end: string, readme: string, contents: string) => {
   if (!readme.includes(start) || !readme.includes(end))
-    throw new Error("Starting and ending string not found");
+    console.log("Starting and ending string not found");
 
   const startString = readme.split(start)[0];
   const endString = readme.split(end)[1];
