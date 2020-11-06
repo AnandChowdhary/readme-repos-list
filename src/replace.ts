388216a4b17857e87d36d121e198f73a23763fbe
Anand Chowdhary <mail@anandchowdhary.com>
2020-11-06 16:23:06 +0530

    :bug: Don't replace if comments don't exist

--- src/replace.ts
+++ src/replace.ts
@@ -1,6 +1,8 @@
 export const replaceContents = (start: string, end: string, readme: string, contents: string) => {
-  if (!readme.includes(start) || !readme.includes(end))
+  if (!readme.includes(start) || !readme.includes(end)) {
     console.log("Starting and ending string not found");
+    return readme;
+  }
 
   const startString = readme.split(start)[0];
   const endString = readme.split(end)[1];
